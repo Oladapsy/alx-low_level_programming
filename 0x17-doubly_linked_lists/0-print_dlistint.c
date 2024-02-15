f632ae925608138aa5d063aca36613810d1218b3
oladapsy <dapoodedeyi@yahoo.com>
2024-02-15 06:28:12 -0800

    retry

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,11 +11,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 1;
+	size_t nodes = 0;
 
-	while (h->next != NULL)
+	while (h)
 	{
 		nodes += 1;
+		printf("%d", h->n);
 		h = h->next;
 	}
 
