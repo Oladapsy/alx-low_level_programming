0d3a1466fe81bd7c5e67f737f9d1ba165f6f6049
Oladapsy <dapoodedeyi@yahoo.com>
2024-03-20 16:33:40 +0100

    find the perimeter of an island of a grid with edges

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,4 +14,17 @@ def island_perimeter(grid):
     The island doesn’t have “lakes”
     (water inside that isn’t connected to the water around the island).
     """
-    
+    height = len(grid)
+    width = len(grid[0])
+    size = 0
+    edges = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return ((size * 4) - (edges * 2))
