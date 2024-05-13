716fe6e6f2391cc74e844058d0a83ed623bed875
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-13 16:36:08 +0100

    not working yet

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,35 +1,6 @@
 #include "search_algos.h"
 
 /**
- * bin_search_help - searches for a value in a sorted array of integers using
- * the Binary search algorithm
- *
- * @array: pointer to the first element of the array
- * @value: the value to search for
- * @left: the let most ndex
- * @right: the right most index
- *
- * Return: index where value is located
- */
-int bin_search_help(int *array, int left, int right, int value)
-{
-
-	size_t mid, size;
-
-	if (array == NULL)
-		return (-1);
-	size = sizeof(array/array[0]);
-	left = 0;
-	right = size - 1;
-	mid = left + (right - 1) / 2;
-	if (array[mid] == value)
-		return (mid);
-	else if (array[mid] > value)
-		return (bin_search_help(array, left, mid - 1, value));
-	else
-		return (bin_search_help(array, mid + 1, right, value));
-}
-/**
  * binary_search - searches for a value in a sorted array of integers using
  * the Binary search algorithm
  *
@@ -41,13 +12,23 @@ int bin_search_help(int *array, int left, int right, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int v_r;
+	size_t i, left, right;
 
 	if (array == NULL)
 		return (-1);
 
-	v_r = bin_search_help(array, 0, array[size - 1], value);
+	for (left = 0; right = size -1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
 
-	return (v_r);
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		else
+			left = i + 1;
+	}
+
+	return (-1);
 }
-
